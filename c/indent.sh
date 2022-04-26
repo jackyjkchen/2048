01ee910f50f5fcb966d9b2c0faf10b708a5af2bb
jiakang.cjk <jiakang.cjk@aliyun.com>
2022-04-26 17:46:30 +0800

    no backup indent

--- c/indent.sh
+++ c/indent.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-indent -i4 -l120 -nut -br -ce -brf -brs -cdw -bad -bap -as -nbc -npsl -nbfda -nbfde -nbs -ncs -npcs -nss -nprs -T row_t -T board_t -T score_t -T score_heur_t -T HANDLE -T DWORD -T COORD -T TCHAR -T eval_state -T lua_State -T JNIEXPORT -T JNICALL -T JNIEnv -T jchar -T jobject  "$@"
+VERSION_CONTROL=none indent -i4 -l120 -nut -br -ce -brf -brs -cdw -bad -bap -as -nbc -npsl -nbfda -nbfde -nbs -ncs -npcs -nss -nprs -T row_t -T board_t -T score_t -T score_heur_t -T HANDLE -T DWORD -T COORD -T TCHAR -T eval_state -T lua_State -T JNIEXPORT -T JNICALL -T JNIEnv -T jchar -T jobject  "$@"
